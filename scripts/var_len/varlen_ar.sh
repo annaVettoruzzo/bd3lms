ebdb97311a624564ec679230cf6305e664580ff8
Anna <anna.vettoruzzo@gmail.com>
2025-07-04 16:48:21 +0200

    modifications for evaluating on snellius

--- scripts/var_len/varlen_ar.sh
+++ scripts/var_len/varlen_ar.sh
@@ -5,7 +5,7 @@
 #SBATCH -N 1                          # Total number of nodes requested
 #SBATCH --get-user-env                # retrieve the users login environment
 #SBATCH --mem=100000                  # server memory requested (per node)
-#SBATCH -t 960:00:00                  # Time limit (hh:mm:ss)
+#SBATCH -t 1:00:00                  # Time limit (hh:mm:ss)
 #SBATCH --partition=gpu          # Request partition
 #SBATCH --constraint="[a5000|a6000|3090|a100]"
 #SBATCH --ntasks-per-node=4
@@ -13,8 +13,25 @@
 #SBATCH --open-mode=append            # Do not overwrite logs
 #SBATCH --requeue                     # Requeue upon preemption
 
-LENGTH=$1
-SEED=$2
+#Loading modules 
+module load 2024 
+module load Python/3.12.3-GCCcore-13.3.0
+module load CUDA/12.6.0
+
+#Environment setup
+export VENV_PATH="/home/avettoruzzo/bd3lms/bd3lm_venv"      ### Refer to your desired virtual environment path
+
+# Check that VENV_PATH is set
+if [ -z "$VENV_PATH" ]; then
+    echo "Error: VENV_PATH is not set. Please export the path to your virtual environment:"
+    exit 1
+fi
+
+# Activate virtual environment
+source "$VENV_PATH/bin/activate"
+
+BLOCK_SIZE=4
+LENGTH=2048
 
 # use model trained w/o eos for variable-length generation
 srun python -su -m main \
--- scripts/var_len/varlen_bd3lm.sh
+++ scripts/var_len/varlen_bd3lm.sh
@@ -5,19 +5,32 @@
 #SBATCH -N 1                          # Total number of nodes requested
 #SBATCH --get-user-env                # retrieve the users login environment
 #SBATCH --mem=100000                  # server memory requested (per node)
-#SBATCH -t 960:00:00                  # Time limit (hh:mm:ss)
-#SBATCH --partition=gpu          # Request partition
-#SBATCH --constraint="[a5000|a6000|3090|a100]"
+#SBATCH -t 1:00:00                  # Time limit (hh:mm:ss)
+#SBATCH --partition=gpu_h100          # Request partition
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4                  # Type/number of GPUs needed
 #SBATCH --open-mode=append            # Do not overwrite logs
 #SBATCH --requeue                     # Requeue upon preemption
 
-nvidia-smi
+#Loading modules 
+module load 2024 
+module load Python/3.12.3-GCCcore-13.3.0
+module load CUDA/12.6.0
 
-LENGTH=$1
-SEED=$2
-BLOCK_SIZE=$3
+#Environment setup
+export VENV_PATH="/home/avettoruzzo/bd3lms/bd3lm_venv"      ### Refer to your desired virtual environment path
+
+# Check that VENV_PATH is set
+if [ -z "$VENV_PATH" ]; then
+    echo "Error: VENV_PATH is not set. Please export the path to your virtual environment:"
+    exit 1
+fi
+
+# Activate virtual environment
+source "$VENV_PATH/bin/activate"
+
+BLOCK_SIZE=4
+LENGTH=2048
 
 srun python -u main.py \
     loader.eval_batch_size=1 \
@@ -25,15 +38,13 @@ srun python -u main.py \
     algo=bd3lm \
     algo.backbone=hf_dit \
     algo.T=5000 \
-    data=openwebtext-split \
+    data=wikitext2 \
     model.length=$LENGTH \
     block_size=$BLOCK_SIZE \
     wandb=null \
     mode=sample_eval \
     eval.checkpoint_path=kuleshov-group/bd3lm-owt-block_size${BLOCK_SIZE} \
     model.attn_backend=sdpa \
-    seed=$SEED \
     sampling.nucleus_p=0.9 \
     sampling.kv_cache=true \
-    sampling.logdir=$PWD/varlen_sample_logs/samples_genlen_bd3lm_blocksize${BLOCK_SIZE} \
-    sampling.var_length=true
+    sampling.logdir=$PWD/sample_logs/samples_genlen_bd3lm_blocksize${BLOCK_SIZE} \
